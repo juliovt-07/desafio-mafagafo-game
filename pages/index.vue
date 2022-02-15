c3f48c07672f7d41b5998f211ce2fdc29dc2dfd8
juliovt-07 <juliocesaar.ma@gmail.com>
2022-02-15 03:00:35 -0300

    ajustar data pra dia 16

--- pages/index.vue
+++ pages/index.vue
@@ -63,7 +63,7 @@ export default {
     var confetti = new ConfettiGenerator(confettiSettings);
     confetti.render();
 
-    var evento  = new Date(2022, 1, 15, 3)
+    var evento  = new Date(2022, 1, 16)
       , atual   = new Date().getTime()
       , duracao = moment.duration(evento - atual, 'milliseconds')
     
